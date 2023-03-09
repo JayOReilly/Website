95700a87fcb81e759057dc8ae13c9463c103d6d8
JayOReilly <99806112+JayOReilly@users.noreply.github.com>
2023-03-09 17:32:21 +0000

    edit5:30

---
+++ src/.next/server/pages/weatherapi.js
@@ -0,0 +1,24 @@
+(() => {
+var exports = {};
+exports.id = 983;
+exports.ids = [983];
+exports.modules = {
+
+/***/ 608:
+/***/ (() => {
+
+throw new Error("Module build failed (from ../node_modules/next/dist/build/babel/loader/index.js):\nError: Cannot find module 'babel/preset-react'\nRequire stack:\n- C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\compiled\\babel\\bundle.js\n- C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\compiled\\babel\\core.js\n- C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\telemetry\\events\\version.js\n- C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\telemetry\\events\\index.js\n- C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\lib\\verifyAndLint.js\n- C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\build\\index.js\n- C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\cli\\next-build.js\n- C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\lib\\commands.js\n- C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\bin\\next\n    at Function.Module._resolveFilename (node:internal/modules/cjs/loader:933:15)\n    at Function.mod._resolveFilename (C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\build\\webpack\\require-hook.js:23:32)\n    at resolve (node:internal/modules/cjs/helpers:108:19)\n    at tryRequireResolve (C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\compiled\\babel\\bundle.js:1:39528)\n    at resolveStandardizedNameForRequire (C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\compiled\\babel\\bundle.js:1:40029)\n    at sync (C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\compiled\\babel\\bundle.js:1:40676)\n    at sync (C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\compiled\\babel\\bundle.js:1910:716601)\n    at C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\compiled\\babel\\bundle.js:1910:717013\n    at Generator.next (<anonymous>)\n    at resolvePreset (C:\\Users\\35383\\Downloads\\Portfolio-main\\Portfolio-main\\node_modules\\next\\dist\\compiled\\babel\\bundle.js:1:38112)");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(608));
+module.exports = __webpack_exports__;
+
+})();
